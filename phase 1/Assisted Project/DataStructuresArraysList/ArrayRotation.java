4311a8e38391be61d58398d8b2bd4802a305c166
KaviBakiyalakshmi <ivak.srmist@gmail.com>
2022-07-29 12:24:12 +0530

    JAVA_FSD

---
+++ phase 1/Assisted Project/DataStructuresArraysList/ArrayRotation.java
@@ -0,0 +1,33 @@
+package DataStructuresArraysList;
+
+public class ArrayRotation 
+{
+   public void rotate(int num[],int k)
+   {
+	   if(k>num.length)
+	   {
+		 //10 divide by 7 will give remainder as 3
+		   
+		   k=k%num.length;
+		   
+		 //create new array with size of given array
+		   
+		   int result[]=new int[num.length];
+		   
+		   for(int i=0;i<k;i++)
+		   {
+			   result[i]=num[num.length-k+i];
+			   //7-3=4 this will skip 4 elements (3rd index) and rest elements  will be rotated
+		   }
+		   int j=0;
+		   
+		   for(int i=k;i<num.length;i++)
+		   {
+			   result[i]=num[j];
+			   j++;
+		   }
+		   System.arraycopy(result, 0, num, 0, num.length);
+		   
+	   }
+   }
+}
